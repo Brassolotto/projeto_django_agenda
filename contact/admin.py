5a976e8beb65ff8ed6a3be2a3fbb4e086390a8f6
Ricardo Brassolotto <23336979+Brassolotto@users.noreply.github.com>
2025-04-01 17:52:38 -0300

    Enhance ContactAdmin configuration with list display, search fields, and ordering options

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,9 @@ from contact import models
 
 @admin.register(models.contact)
 class ContactAdmin(admin.ModelAdmin):
-   ...
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date')
+    search_fields = ('first_name', 'last_name', 'phone', 'email')
+    # list_filter = ('created_date',)
+    ordering = ('-created_date',)
+    list_per_page = 20
+    list_max_show_all = 100
